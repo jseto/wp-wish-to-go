3490a27277f0b40d4df0fc03eba061b4470b0a9d
Josep Seto <jsetojseto@gmail.com>
2020-03-30 04:15:14 +0200

    settings page done

---
+++ wish-to-go/settings-page.php
@@ -0,0 +1,87 @@
+<?php
+add_action( 'admin_menu', 'wtg_add_admin_menu' );
+add_action( 'admin_init', 'wtg_settings_init' );
+
+
+function wtg_add_admin_menu(  ) { 
+
+	add_options_page( 'Wish To Go', 'Wish To Go', 'manage_options', 'wish-to-go', 'wtg_options_page' );
+
+}
+
+
+function wtg_settings_init(  ) { 
+
+	register_setting( 'pluginPage', 'wtg_settings' );
+
+	add_settings_section(
+		'wtg_pluginPage_section', 
+		__( 'Settings', 'Wish To Go' ), 
+		'wtg_settings_section_callback', 
+		'pluginPage'
+	);
+
+	add_settings_field( 
+		'wtg_setting_show_wish_counter', 
+		__( 'Show Wish Counter', 'Wish To Go' ), 
+		'wtg_setting_show_wish_counter_render', 
+		'pluginPage', 
+		'wtg_pluginPage_section' 
+	);
+
+	// add_settings_field( 
+	// 	'wtg_setting_api_key', 
+	// 	__( 'API Key', 'Wish To Go' ), 
+	// 	'wtg_setting_api_key_render', 
+	// 	'pluginPage', 
+	// 	'wtg_pluginPage_section' 
+	// );
+
+
+}
+
+
+function wtg_setting_show_wish_counter_render(  ) { 
+
+	$options = get_option( 'wtg_settings' );
+	?>
+	<input type='checkbox' name='wtg_settings[wtg_setting_show_wish_counter]' <?php checked( $options['wtg_setting_show_wish_counter'], 1 ); ?> value='1'>
+	<?php
+
+}
+
+
+function wtg_setting_api_key_render(  ) { 
+
+	$options = get_option( 'wtg_settings' );
+	?>
+	<input type='text' name='wtg_settings[wtg_setting_api_key]' value='<?php echo $options['wtg_setting_api_key']; ?>'>
+	<?php
+
+}
+
+
+function wtg_settings_section_callback(  ) { 
+
+	// echo __( 'This section description', 'Wish To Go' );
+
+}
+
+
+function wtg_options_page(  ) { 
+
+		?>
+		<form action='options.php' method='post'>
+
+			<h2>Wish To Go</h2>
+
+			<?php
+			settings_fields( 'pluginPage' );
+			do_settings_sections( 'pluginPage' );
+			submit_button();
+			?>
+
+		</form>
+		<?php
+
+}
--- wish-to-go/wish-to-go.php
+++ wish-to-go/wish-to-go.php
@@ -19,6 +19,8 @@ if ( ! defined( 'WPINC' ) ) {                                                   
 	die;
 }
 
+include 'settings-page.php';
+
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
@@ -38,13 +40,104 @@ class WishToGo {
   }
 
 	function enqueueFrontEndScripts() {
+    wp_enqueue_style( 'wish-to-go-local-css', plugins_url( '/style.css', __FILE__ ) );
 		wp_register_script( 'wish-to-go-js', 'https://wish-to-go.com/wish-to-go.main.js', null, null, true );
 		wp_enqueue_script( 'wish-to-go-js' );
   }
 
   function register() {
+    $pluginName = plugin_basename( __FILE__ );
+
+    add_filter( "plugin_action_links_$pluginName", array( $this, 'settings_menu' ) );
+
     add_action( 'admin_enqueue_scripts', array( $this, 'enqueueAdminScripts' ) );
     add_action( 'wp_enqueue_scripts', array( $this, 'enqueueFrontEndScripts' ) );
+
+    add_action('admin_footer-post-new.php', array( $this, 'addCustomFieldsMenuItems' ) );
+    add_action('admin_footer-post.php', array( $this, 'addCustomFieldsMenuItems' ) );
+
+    add_filter( 'the_title', array( $this, 'appendWishWidgetToTitle' ), 10, 2 );
+
+    $options = get_option('wtg_settings', false );
+    if ( $options && $options['wtg_setting_show_wish_counter'] ) {
+      add_action( 'wp_footer', array( $this, 'appendWishCounterToContent' ), 10, 2 );
+    }
+    
+    $this->registerShortCodes();
+  }
+
+  function registerShortCodes() {
+    add_shortcode('wishwidget', array( $this, 'wishWidget' ) );
+  }
+
+  function settings_menu( $links ) {
+    $pluginLinks = array(
+      '<a href="'. admin_url( 'admin.php?page=wish-to-go' ).'">Settings</a>'
+    );
+
+    return array_merge( $pluginLinks, $links );
+  }
+
+  function wishWidget( $attributes = [], $content = null, $tag = '' ) {
+    $att = '';
+  
+    if ( $attributes ) {
+      foreach ( $attributes as $key => $value ) {
+        $att = $att . $key . '="' . $value . '" ';
+      }
+    }
+
+    return "<wishwidget $att></wishwidget>";
+  }
+
+  function appendWishCounterToContent() {
+    echo "
+      <div class=\"wish-to-go\">
+        <div class=\"stick-to-bottom\">  
+          <wishcounterwidget></wishcounterwidget>
+        </div>
+      </div>
+    ";
+  }
+
+  function appendWishWidgetToTitle( $title, $id ) {
+    if ( is_admin() ) {
+      return $title;
+    }
+
+    $post = get_post( $id );
+
+    if ( ! ( in_the_loop() && $post instanceof WP_Post 
+      && ( $post->post_type == 'post' 
+        || $post->post_type == 'page'  //TODO: Make as option
+      ) ) ) {
+      return $title;
+    }
+
+    $country = get_post_meta( $id, 'country', true );
+    $city = get_post_meta( $id, 'city', true );
+    $activity = get_post_meta( $id, 'activity', true );
+    $picture = get_the_post_thumbnail_url( $id, 'large' );
+    $postURL = get_permalink( $id );
+
+    if ( ! $country
+      // || is_front_page() //TODO: Make as option
+      ) {
+      return $title;
+    }
+
+    $widget = "
+      <wishwidget
+        country=\"$country\"
+        city=\"$city\"
+        activity=\"$activity\"
+        picture=\"$picture\"
+        post=\"$postURL\"
+      >
+      </wishwidget>
+    ";
+
+    return $title . $widget;
   }
 
   function activate(){
@@ -55,9 +148,82 @@ class WishToGo {
   function deactivate(){
     flush_rewrite_rules();
   }
+
+  /**
+ * Programatically add custom fields.
+ *
+ * @see http://wordpress.stackexchange.com/questions/98269/programatically-add-options-to-add-new-custom-field-dropdown/
+ */
+
+  function addCustomFieldsMenuItems() {
+    if (isset($GLOBALS['post'])) {
+      $post_type = get_post_type($GLOBALS['post']);
+
+      if (post_type_supports($post_type, 'custom-fields')) {
+        ?>
+          <script>
+              // Cache:
+              var $metakeyinput = jQuery('#metakeyinput'),
+                  $metakeyselect = jQuery('#metakeyselect');
+              // Does the default input field exist and is it visible?
+              if ($metakeyinput.length && ( ! $metakeyinput.hasClass('hide-if-js'))) {
+                  // Hide it:
+                  $metakeyinput.addClass('hide-if-js'); // Using WP admin class.
+                  // ... and create the select box:
+                  $metakeyselect = jQuery('<select id="metakeyselect" name="metakeyselect">').appendTo('#newmetaleft');
+                  // Add the default select value:
+                  $metakeyselect.append('<option value="#NONE#">— Select —</option>');
+              }
+              // Does "country" already exist?
+              if (jQuery("[value='country']").length < 1) {
+                  // Add option:
+                  $metakeyselect.append("<option value='country'>country</option>");
+              }
+              if (jQuery("[value='city']").length < 1) {
+                  $metakeyselect.append("<option value='city'>city</option>");
+              }
+              if (jQuery("[value='activity']").length < 1) {
+                  $metakeyselect.append("<option value='activity'>activity</option>");
+              }
+          </script>
+        <?php
+      }
+    }
+  }
 }
+  
+class WishToGoWidget extends WP_Widget {
+  public function __construct() {
+    parent::__construct(
+      'WishToGoWidget',
+      __( 'Wish To Go Travel Planner', 'text_domain' ),
+      array(
+        'customize_selective_refresh' => true,
+      )
+    );
+  }
+
+  public function form( $instance ) {
+  }   
+  
+  public function widget( $args, $instance ) {
+    echo "
+      <div style=\"margin-bottom: 2em;\">
+        <travelplanwidget></travelplanwidget>
+      </div>
+    ";
+  }
+ 
+}
+
 
 $wishToGo = new WishToGo();
 $wishToGo->register();
 register_activation_hook( __FILE__, array( $wishToGo, 'activate' ) );
 register_deactivation_hook( __FILE__, array( $wishToGo, 'deactivate' ) );
+
+function register_wtg_widget() {
+  register_widget( 'WishToGoWidget' );
+}
+
+add_action( 'widgets_init', 'register_wtg_widget' );
